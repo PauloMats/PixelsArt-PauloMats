da31f99ac55e533089f1e577bdcb739c2ede0871
Paulo Mateus <pm.mats98@gmail.com>
2023-05-17 16:53:01 -0300

    tentativa de resolução correta do requisito 2

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
 const colorBlack = document.getElementsByClassName('color')[0];
 
-colorBlack = colorBlack.style.background: 'black';
+colorBlack.style.backgroundColor = 'black';
+colorBlack.style.color = 'white';
+
+const color1 = document.getElementsByClassName('color')[1];
+color1.style.backgroundColor = 'red';
+
+const color2 = document.getElementsByClassName('color')[2];
+color2.style.backgroundColor = 'green';
+
+const color3 = document.getElementsByClassName('color')[3];
+color3.style.backgroundColor = 'blue';
+color3.style.color = 'white';
+
