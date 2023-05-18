12461cd46262cef2bcad9a995f22a3b14c7644fb
Paulo Mateus <pm.mats98@gmail.com>
2023-05-18 13:22:51 -0300

    ah, o bug... ah o maravilhoso bug das cores

--- script.js
+++ script.js
@@ -13,12 +13,6 @@ const color3 = document.getElementsByClassName('color')[3];
 color3.style.backgroundColor = 'green';
 
 // 4 - Adicione um botão para gerar cores aleatórias para a paleta de cores
-
-window.onload = function(){ 
-  guardarCor();
-};
-
-
 function randomColor() {
   const codColor = '0123456789ABCDEF';
   let color = '#';
@@ -52,22 +46,21 @@ const guardarCor = () => {
     colors[i].style.backgroundColor = restColor[i];
   }
 };
+window.onload = function(){ 
+  guardarCor();
+};
 
 // 6 - Adicionar um quadro com 25 pixels (5x5)
-//  const quadro = document.createElement('section');
-//  quadro.className = 'pixel-board';
-//  const main = document.querySelector('main');
-//  main.appendChild(quadro);
 
-//  const espacosQuadro () => {
-//       for (let i = 0; i < 6; i+= 1){
-//          const espacos = document.createElement('div');
-//         espacos.tagName = 'pixel';
-//         espacos.style.height = 40px;
-//         espacos.style.width = 40px;
-//        quadro.appendChild(espacos);
-//      }
+const quadro = document.createElement('section');
+quadro.tagName = 'pixel-board';
+const main = document.querySelector('main');
+main.appendChild(quadro);
 
-//  }
+for (let i = 0; i < 6; i += 1) {
+  const linhas = document.createElement('div');
+  linhas.className = 'pixel';
+  quadro.appendChild(linhas);
+}
 
 // 7 - Faça com que cada pixel do quadro tenha largura e altura de 40 pixels e borda preta de 1 pixel de espessura
