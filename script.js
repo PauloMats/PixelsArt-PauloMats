8334934a0898ac199b069c98f94c8324fd449f60
Paulo Mateus <pm.mats98@gmail.com>
2023-05-18 09:46:13 -0300

    o maravilhoso codigo que salva apenas a cor após o f5, ele meio que prevê o futuro

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-//2 e 3 Adiciona paleta com 4 cores diferentes, adiciona a primeira cor como preta.
+// 2 e 3 Adiciona paleta com 4 cores diferentes, adiciona a primeira cor como preta.
 
 const colorBlack = document.getElementsByClassName('color')[0];
 colorBlack.style.backgroundColor = 'black';
@@ -12,9 +12,13 @@ color2.style.backgroundColor = 'yellow';
 const color3 = document.getElementsByClassName('color')[3];
 color3.style.backgroundColor = 'green';
 
+// 4 - Adicione um botão para gerar cores aleatórias para a paleta de cores
+
+window.onload = function(){ 
+  guardarCor();
+};
 
 
-//4 - Adicione um botão para gerar cores aleatórias para a paleta de cores
 function randomColor() {
   const codColor = '0123456789ABCDEF';
   let color = '#';
@@ -29,39 +33,41 @@ const colors = document.querySelectorAll('.color');
 const button = document.getElementById('button-random-color');
 
 button.addEventListener('click', () => {
+  const randomColorLs = [];
+
   for (let i = 1; i < colors.length; i += 1) {
+    randomColorLs.push(colors[i].style.backgroundColor);
     const coresRandom = colors[i];
     coresRandom.style.backgroundColor = randomColor();
   }
-  localStorage.setItem('restoreColor', color);
+  localStorage.setItem('restoreColor', JSON.stringify(randomColorLs));
 });
 
+// 5 - Implemente uma função usando localStorage para que a paleta de cores gerada aleatoriamente seja mantida após recarregar a página
 
-
-//5 - Implemente uma função usando localStorage para que a paleta de cores gerada aleatoriamente seja mantida após recarregar a página
-  
 const guardarCor = () => {
-  const restColor = localStorage.getItem('restoreColor')
-}
-coresRandom.style.backgroundColor = guardarCor();
+  const restColor = JSON.parse(localStorage.getItem('restoreColor'));
 
+  for (let i = 1; i < colors.length; i += 1) {
+    colors[i].style.backgroundColor = restColor[i];
+  }
+};
 
+// 6 - Adicionar um quadro com 25 pixels (5x5)
+//  const quadro = document.createElement('section');
+//  quadro.className = 'pixel-board';
+//  const main = document.querySelector('main');
+//  main.appendChild(quadro);
 
-//6 - Adicionar um quadro com 25 pixels (5x5)
-// const quadro = document.createElement('section');
-// quadro.className = 'pixel-board';
-// main.appendChild(quadro);
+//  const espacosQuadro () => {
+//       for (let i = 0; i < 6; i+= 1){
+//          const espacos = document.createElement('div');
+//         espacos.tagName = 'pixel';
+//         espacos.style.height = 40px;
+//         espacos.style.width = 40px;
+//        quadro.appendChild(espacos);
+//      }
 
-// const espacosQuadro () => {
-//      for (let i = 0; i < 6; i+= 1){
-//         const espacos = document.createElement('div');
-//        espacos.tagName = 'pixel';
-//        espacos.style.height = 40px;
-//        espacos.style.width = 40px;
-//       quadro.appendChild(espacos);
-//     }
-    
-// }
+//  }
 
-
-//7 - Faça com que cada pixel do quadro tenha largura e altura de 40 pixels e borda preta de 1 pixel de espessura
+// 7 - Faça com que cada pixel do quadro tenha largura e altura de 40 pixels e borda preta de 1 pixel de espessura
