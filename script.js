a6b5996e7820f41e0e4f71d8208c15ffc98c3f47
Paulo Mateus <pm.mats98@gmail.com>
2023-05-18 08:26:51 -0300

    adiciona margins no css e novas cores iniciais para sinalizar um semáforo. Vc me pergunta o por que, e eu te respondo: Por que não?

--- script.js
+++ script.js
@@ -7,10 +7,11 @@ const color1 = document.getElementsByClassName('color')[1];
 color1.style.backgroundColor = 'red';
 
 const color2 = document.getElementsByClassName('color')[2];
-color2.style.backgroundColor = 'green';
+color2.style.backgroundColor = 'yellow';
 
 const color3 = document.getElementsByClassName('color')[3];
-color3.style.backgroundColor = 'blue';
+color3.style.backgroundColor = 'green';
+
 
 
 //4 - Adicione um botão para gerar cores aleatórias para a paleta de cores
@@ -36,25 +37,31 @@ button.addEventListener('click', () => {
 });
 
 
-//5 - Implemente uma função usando localStorage para que a paleta de cores gerada aleatoriamente seja mantida após recarregar a página
 
+//5 - Implemente uma função usando localStorage para que a paleta de cores gerada aleatoriamente seja mantida após recarregar a página
+  
 const guardarCor = () => {
   const restColor = localStorage.getItem('restoreColor')
 }
+coresRandom.style.backgroundColor = guardarCor();
 
 
-//6 - Adicionar um quadro 5 x 5 com 25 pixels
-const quadro = document.createElement('section');
-quadro.className = 'pixel-board';
-main.appendChild(quadro);
 
-const espacosQuadro () => {
-     for (let i = 0; i < 6; i+= 1){
-        const espacos = document.createElement('div');
-       espacos.tagName = 'pixel';
-       espacos.style.height = 40px;
-       espacos.style.width = 40px;
-      quadro.appendChild(espacos);
-    }
+//6 - Adicionar um quadro com 25 pixels (5x5)
+// const quadro = document.createElement('section');
+// quadro.className = 'pixel-board';
+// main.appendChild(quadro);
+
+// const espacosQuadro () => {
+//      for (let i = 0; i < 6; i+= 1){
+//         const espacos = document.createElement('div');
+//        espacos.tagName = 'pixel';
+//        espacos.style.height = 40px;
+//        espacos.style.width = 40px;
+//       quadro.appendChild(espacos);
+//     }
     
-}
+// }
+
+
+//7 - Faça com que cada pixel do quadro tenha largura e altura de 40 pixels e borda preta de 1 pixel de espessura
