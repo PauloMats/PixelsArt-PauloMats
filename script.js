6600a0141f68453e0f8c1f8464b8180b49478c0e
Paulo Mateus <pm.mats98@gmail.com>
2023-05-19 13:27:21 -0300

    depois de muuuuito tempo tentando arrumar o codigo, descobri que a funcao para o botão limpar os quadros estava quebrando o código.

--- script.js
+++ script.js
@@ -1,38 +1,15 @@
 // 2 e 3 Adiciona paleta com 4 cores diferentes, adiciona a primeira cor como preta.
 
-const colorBlack = document.getElementsByClassName('color')[0];
-colorBlack.style.backgroundColor = 'black';
+const colors = document.getElementsByClassName('color');
+colors[0].style.backgroundColor = 'black';
+colors[1].style.backgroundColor = 'red';
+colors[2].style.backgroundColor = 'yellow';
+colors[3].style.backgroundColor = 'green';
 
-const color1 = document.getElementsByClassName('color')[1];
-color1.style.backgroundColor = 'red';
+// 4 - Adicione um botão para gerar cores aleatórias para a paleta de cores e adicionei o botão limpar cores
 
-const color2 = document.getElementsByClassName('color')[2];
-color2.style.backgroundColor = 'yellow';
-
-const color3 = document.getElementsByClassName('color')[3];
-color3.style.backgroundColor = 'green';
-
-// 4 - Adicione um botão para gerar cores aleatórias para a paleta de cores
-
-const colors = document.querySelectorAll('.color');
+const botaoClear = document.getElementById('#clear-board');
 const button = document.getElementById('button-random-color');
-
-button.addEventListener('click', () => {
-  const colorPalette = [];
-  for (let i = 0; i < colors.length; i += 1) {
-    if (i !== 0) {
-      const r = Math.floor(Math.random() * 256);
-      const g = Math.floor(Math.random() * 256);
-      const b = Math.floor(Math.random() * 256);
-      colors[i].style.backgroundColor = `rgb(${r}, ${g}, ${b})`;
-    }
-    colorPalette.push(colors[i].style.backgroundColor);
-  }
-  localStorage.setItem('colorPalette', JSON.stringify(colorPalette));
-});
-
-// 5 - Implemente uma função usando localStorage para que a paleta de cores gerada aleatoriamente seja mantida após recarregar a página
-
 const pegarCorPixelBoard = document.getElementById('pixel-board');
 
 const pegarCor = () => {
@@ -50,20 +27,73 @@ const startPalet = () => {
   }
 };
 
+button.addEventListener('click', () => {
+  const colorPalette = [];
+  for (let i = 0; i < colors.length; i += 1) {
+    if (i !== 0) {
+      const r = Math.floor(Math.random() * 256);
+      const g = Math.floor(Math.random() * 256);
+      const b = Math.floor(Math.random() * 256);
+      colors[i].style.backgroundColor = `rgb(${r}, ${g}, ${b})`;
+    }
+    colorPalette.push(colors[i].style.backgroundColor);
+  }
+  localStorage.setItem('colorPalette', JSON.stringify(colorPalette));
+});
+
+// 8-10 Função para selecionar cor e para preencher um pixel do quadro com a cor selecionada.
+
+const removeCor = () => {
+  for (let index = 0; index < colors.length; index += 1) {
+    colors[index].classList.remove('selected');
+  }
+};
+
+const selecionaCor = () => {
+  colors[0].classList.add('selected');
+  for (let index = 0; index < colors.length; index += 1) {
+    colors[index].addEventListener('click', () => {
+      removeCor();
+      colors[index].classList.add('selected');
+    });
+  }
+};
+
+const pintarPixel = () => {
+  let corSelecionada;
+  for (let index = 0; index < colors.length; index += 1) {
+    if (colors[index].className === 'color selected') {
+      corSelecionada = colors[index].style.backgroundColor;
+    }
+  }
+  return corSelecionada;
+};
+
+const pintar = () => {
+  const unidadePixel = document.getElementsByClassName('pixel');
+  for (let index = 0; index < unidadePixel.length; index += 1) {
+    unidadePixel[index].addEventListener('click', () => {
+      unidadePixel[index].style.backgroundColor = pintarPixel();
+    });
+  }
+};
+
+function clearQuadroPixels() {
+  const unidadePixel = document.getElementsByClassName('pixel');
+  for (let index = 0; index < unidadePixel.length; index += 1) {
+    unidadePixel[index].style.backgroundColor = 'rgb(255,255,255)';
+  }
+};
+
 function setarCorPixel() {
   localStorage.setItem('pixelBoard', pegarCorPixelBoard.innerHTML);
 }
 
-// 8 - Defina a cor preta como cor inicial da paleta de cores
-
-colorBlack.classList.add('selected');
-
-// 9 - Crie uma função para selecionar uma cor na paleta de cores
-pegarCorPixelBoard.addEventListener('click', setarCorPixel);
-botaoClear.addEventListener('click', clearQuadroPixels);
-
-// 10 - Crie uma função que permita preencher um pixel do quadro com a cor selecionada na paleta de cores
+pegarCorPixelBoard.addEventListener('click', setarCorPixel());
+// botaoClear.addEventListener('click', clearQuadroPixels());
 
 window.onload = () => {
   startPalet();
+  selecionaCor();
+  pintar();
 };
